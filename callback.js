4026de40e6e6d14aff23587c4fd95db887169a20
Hari555Y <78408845+Hari555Y@users.noreply.github.com>
2022-06-17 08:59:10 +0530

    Add files via upload

---
+++ callback.js
@@ -0,0 +1,8 @@
+window.onload=  function(){
+    function cla(){
+        console.log("Hello world");
+    }
+document.getElementById("bt").addEventListener("click",cla);
+}
+
+//button.addEventListener('click',callback);
