9d08e0fdcc715597313a6b89244a6fcb151ad10b
Hari555Y <78408845+Hari555Y@users.noreply.github.com>
2022-06-21 14:11:35 +0530

    Add files via upload
    my folder containing all the react files.

---
+++ react/App copy.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  let myvariable = 134;
+  return (
+    <>
+    <h1>MY app </h1>
+    <p>my app works</p></>
+  );
+}
+
+export default App;
